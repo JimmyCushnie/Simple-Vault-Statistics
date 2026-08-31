c196f7c6df4c2a684fc24e9626999e1fc537add3
Jimmy Cushnie <git@jimmycushnie.com>
2026-08-31 10:12:47 -0400

    Reorganize settings

--- src/settings.ts
+++ src/settings.ts
@@ -59,117 +59,129 @@ export class SimpleVaultStatisticsSettingsTab extends PluginSettingTab {
 	getSettingDefinitions(): SettingDefinitionItem<keyof SimpleVaultStatisticsSettings>[] {
 		return [
 			{
-				name: 'Display style',
-				desc: 'Controls how statistics are displayed in the popup.',
-				control: {
-					type: 'dropdown',
-					key: 'displayStyle',
-					options: DISPLAY_STYLE_LABELS,
-				},
+				type: 'group',
+				heading: 'General',
+				items: [
+					{
+						name: 'Display style',
+						desc: 'Controls how statistics are displayed in the popup.',
+						control: {
+							type: 'dropdown',
+							key: 'displayStyle',
+							options: DISPLAY_STYLE_LABELS,
+						},
+					},
+					{
+						name: 'Count .txt files as notes',
+						desc: 'If enabled, .txt files will be counted in "note count" instead of "other files count", and their contents will contribute to the word count and character count.',
+						control: {
+							type: 'toggle',
+							key: 'txtFilesCountAsNotes',
+						},
+					},
+				],
 			},
 			{
-				name: 'Show vault name',
-				desc: 'Shows the name of your vault as a small header above the statistics.',
-				control: {
-					type: 'toggle',
-					key: 'showVaultName',
-				},
-			},
-			{
-				name: 'Show notes count',
-				desc: 'Shows the total number of notes in your vault.',
-				control: {
-					type: 'toggle',
-					key: 'showNotesCount',
-				},
-			},
-			{
-				name: 'Show word count',
-				desc: 'Shows the total number of words in all the notes in your vault. Does not count words in frontmatter blocks.',
-				control: {
-					type: 'toggle',
-					key: 'showWordCount',
-				},
-			},
-			{
-				name: 'Show character count',
-				desc: 'Shows the total number of characters in all the notes in your vault, including characters in frontmatter blocks.',
-				control: {
-					type: 'toggle',
-					key: 'showCharacterCount',
-				},
-			},
-			{
-				name: 'Count .txt files as notes',
-				desc: 'If enabled, .txt files will be counted in "note count" instead of "other files count", and their contents will contribute to the word count and character count.',
-				control: {
-					type: 'toggle',
-					key: 'txtFilesCountAsNotes',
-				},
-			},
-			{
-				name: 'Show folders count',
-				desc: 'Shows the total number of folders in your vault.',
-				control: {
-					type: 'toggle',
-					key: 'showFoldersCount',
-				},
-			},
-			{
-				name: 'Show other files count',
-				desc: 'Shows the total number of non-note files in your vault, such as images, PDFs, and canvases.',
-				control: {
-					type: 'toggle',
-					key: 'showOtherFilesCount',
-				},
-			},
-			{
-				name: 'Show internal links count',
-				desc: 'Shows the total number of links in all the notes in your vault that point to other files in your vault.',
-				control: {
-					type: 'toggle',
-					key: 'showInternalLinksCount',
-				},
-			},
-			{
-				name: 'Show external links count',
-				desc: 'Shows the total number of links in all the notes in your vault that point outside your vault, such as web URLs.',
-				control: {
-					type: 'toggle',
-					key: 'showExternalLinksCount',
-				},
-			},
-			{
-				name: 'Show footnotes count',
-				desc: 'Shows the total number of footnotes in all the notes in your vault.',
-				control: {
-					type: 'toggle',
-					key: 'showFootnotesCount',
-				},
-			},
-			{
-				name: 'Show tags count',
-				desc: 'Shows the total number of times a note has been given a tag.',
-				control: {
-					type: 'toggle',
-					key: 'showTagsCount',
-				},
-			},
-			{
-				name: 'Show checked checkboxes count',
-				desc: 'Shows the total number of checked checkboxes in all the notes in your vault.',
-				control: {
-					type: 'toggle',
-					key: 'showCheckedCheckboxesCount',
-				},
-			},
-			{
-				name: 'Show unchecked checkboxes count',
-				desc: 'Shows the total number of unchecked checkboxes in all the notes in your vault.',
-				control: {
-					type: 'toggle',
-					key: 'showUncheckedCheckboxesCount',
-				},
+				type: 'group',
+				heading: 'Popup contents',
+				items: [
+					{
+						name: 'Show vault name',
+						desc: 'Shows the name of your vault as a small header above the statistics.',
+						control: {
+							type: 'toggle',
+							key: 'showVaultName',
+						},
+					},
+					{
+						name: 'Show notes count',
+						desc: 'Shows the total number of notes in your vault.',
+						control: {
+							type: 'toggle',
+							key: 'showNotesCount',
+						},
+					},
+					{
+						name: 'Show word count',
+						desc: 'Shows the total number of words in all the notes in your vault. Does not count words in frontmatter blocks.',
+						control: {
+							type: 'toggle',
+							key: 'showWordCount',
+						},
+					},
+					{
+						name: 'Show character count',
+						desc: 'Shows the total number of characters in all the notes in your vault, including characters in frontmatter blocks.',
+						control: {
+							type: 'toggle',
+							key: 'showCharacterCount',
+						},
+					},
+					{
+						name: 'Show folders count',
+						desc: 'Shows the total number of folders in your vault.',
+						control: {
+							type: 'toggle',
+							key: 'showFoldersCount',
+						},
+					},
+					{
+						name: 'Show other files count',
+						desc: 'Shows the total number of non-note files in your vault, such as images, PDFs, and canvases.',
+						control: {
+							type: 'toggle',
+							key: 'showOtherFilesCount',
+						},
+					},
+					{
+						name: 'Show internal links count',
+						desc: 'Shows the total number of links in all the notes in your vault that point to other files in your vault.',
+						control: {
+							type: 'toggle',
+							key: 'showInternalLinksCount',
+						},
+					},
+					{
+						name: 'Show external links count',
+						desc: 'Shows the total number of links in all the notes in your vault that point outside your vault, such as web URLs.',
+						control: {
+							type: 'toggle',
+							key: 'showExternalLinksCount',
+						},
+					},
+					{
+						name: 'Show footnotes count',
+						desc: 'Shows the total number of footnotes in all the notes in your vault.',
+						control: {
+							type: 'toggle',
+							key: 'showFootnotesCount',
+						},
+					},
+					{
+						name: 'Show tags count',
+						desc: 'Shows the total number of times a note has been given a tag.',
+						control: {
+							type: 'toggle',
+							key: 'showTagsCount',
+						},
+					},
+					{
+						name: 'Show checked checkboxes count',
+						desc: 'Shows the total number of checked checkboxes in all the notes in your vault.',
+						control: {
+							type: 'toggle',
+							key: 'showCheckedCheckboxesCount',
+						},
+					},
+					{
+						name: 'Show unchecked checkboxes count',
+						desc: 'Shows the total number of unchecked checkboxes in all the notes in your vault.',
+						control: {
+							type: 'toggle',
+							key: 'showUncheckedCheckboxesCount',
+						},
+					},
+				],
 			},
 		];
 	}
